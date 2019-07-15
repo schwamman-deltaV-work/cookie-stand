d39f06a58b013a28a1f827680cde3aa8628be261
Jonathon Schwamman <schwamman@msn.com>
2019-07-15 14:03:03 -0500

    Created all other object literals using same format from original object

--- js/app.js
+++ js/app.js
@@ -7,10 +7,63 @@ var firstAndPike = {
         return numCustomers;
     },
     cookiesPurchased: function() {
+        var numPurchased = Math.floor(this.randomCustomers() * this.avgCookies);
+        return numPurchased;
+    }
+}
+
+var seaTacAirport = {
+    minCustomers: 3,
+    maxCustomers: 24,
+    avgCookies: 1.2,
+    randomCustomers: function() {
+        var numCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) ) + this.minCustomers;
+        return numCustomers;
+    },
+    cookiesPurchased: function() {
         var numPurchased = this.randomCustomers() * this.avgCookies;
         return numPurchased;
     }
 }
 
-console.log(firstAndPike.randomCustomers());
-console.log(firstAndPike.cookiesPurchased());
+var seattleCenter = {
+    minCustomers: 11,
+    maxCustomers: 38,
+    avgCookies: 3.7,
+    randomCustomers: function() {
+        var numCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) ) + this.minCustomers;
+        return numCustomers;
+    },
+    cookiesPurchased: function() {
+        var numPurchased = this.randomCustomers() * this.avgCookies;
+        return numPurchased;
+    }
+}
+
+var capitolHill = {
+    minCustomers: 20,
+    maxCustomers: 38,
+    avgCookies: 2.3,
+    randomCustomers: function() {
+        var numCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) ) + this.minCustomers;
+        return numCustomers;
+    },
+    cookiesPurchased: function() {
+        var numPurchased = this.randomCustomers() * this.avgCookies;
+        return numPurchased;
+    }
+}
+
+var alki = {
+    minCustomers: 2,
+    maxCustomers: 16,
+    avgCookies: 4.6,
+    randomCustomers: function() {
+        var numCustomers = Math.floor(Math.random() * (this.maxCustomers - this.minCustomers + 1) ) + this.minCustomers;
+        return numCustomers;
+    },
+    cookiesPurchased: function() {
+        var numPurchased = this.randomCustomers() * this.avgCookies;
+        return numPurchased;
+    }
+}
